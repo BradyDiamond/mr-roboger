1ea0f6b82c9b4086412d854667183a38bdc0a07e
Brady Diamond <brdydmnd@gmail.com>
2022-04-01 15:18:20 -0700

    troubleshoot UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -41,8 +41,9 @@ function replaceNumber(element) {
 $(document).ready(function(){
   $("form#input").submit(function(event) {
     event.preventDefault();
+    const replacedNumber = replaceNumber
     let num = $("#number")
-    $("#output").text(replacedArray(num));
+    $("#output").text(replacedArray);
     
   });
 });
