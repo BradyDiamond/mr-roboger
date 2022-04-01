9e39f5f962e36ec5968d6b1b90913695063e54e1
Brady Diamond <brdydmnd@gmail.com>
2022-04-01 11:06:26 -0700

    BUGFIX test 2

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,26 @@ function inputNumber(number) {
   return(newArray)
 }
 
+function replaceNumber(number)
+const newArray = number.split('')
+newArray.forEach(function(number) {
+  if (number.includes(1)) {
+      newArray.push("beep");
+  // } else if (rank >= 2000) {
+  //     newArray.push('mid');
+  // } else {
+  //     newArray.push('junior');
+  // }
+}
 
-//continue to log tests
+console.log(newArray);
 
 
-
+// if (number.includes(1)) {
+//   newArray.push("beep");
+// }
+// replaceNumber(12)
+// }
 
 
 
