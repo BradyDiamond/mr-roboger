bfa4b73783d04a17065dd520c07a7c25da665e4e
Brady Diamond <brdydmnd@gmail.com>
2022-04-01 14:48:43 -0700

    add initial HTML

--- README.md
+++ README.md
@@ -6,22 +6,22 @@ Expected Output: [0, 1, 2, 3, 4, 5]
 
 Test: It should return an array of numbers from 0 to the user's inputted number as a string
 Code: inputNumber(5);
-Expected Output: [0, 1, 2, 3, 4, 5]
+Expected Output: ['0', '1', '2', '3', '4', '5']
 
 Describe: replaceNumber()
 
 Test: It should replace 1 with "beep"
 Code: replaceNumber(1);
-Expected Output: ["beep"]
+Expected Output: ['beep']
 
 Test: It should replace 2 with "boop"
 Code: replaceNumber(2);
-Expected Output: ["boop"]
+Expected Output: ['boop']
 
 Test: It should replace 3 with "won't you be my neibor"
 Code: replaceNumber(3)
-Expected Output: ["boop"]
+Expected Output: ['boop']
 
 Test: It should NOT alter numbers without 1/2/3
 Code: replaceNumber(4)
-Expected Output: ["4"]
+Expected Output: ['4']
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
+
+
 //Business logic
 
 function inputNumber(element) {
@@ -6,19 +12,19 @@ function inputNumber(element) {
   newArray.push(i);
   }
   let strArray = newArray.toString();
-  const splitArray = strArray.split(",");
+  const splitArray = strArray.split(',');
   return splitArray;
 }
 function replaceNumber(element) {
   const replacedArray = [];
   const splitArray = inputNumber(element);
   splitArray.forEach(function(element) {
-  if (element.includes('1')) {
-    replacedArray.push('beep');
+  if (element.includes('3')) {
+    replacedArray.push("won't you be my neibor?");
   } else if (element.includes ('2')) { 
-    replacedArray.push('boop');
-  } else if (element.includes ('3')) { 
-    replacedArray.push("won't you be my neibor");
+    replacedArray.push('boop!');
+  } else if (element.includes ('1')) { 
+    replacedArray.push('beep!');
   } else { 
   replacedArray.push(element);
 }
@@ -26,22 +32,20 @@ function replaceNumber(element) {
   return replacedArray;
 }
 
-// if (number.includes(1)) {
-//   newArray.push("beep");
-// }
-// replaceNumber()
-// }
-
-
-
-
-
-
-
 
 
 //UI Logic
 
 
-$(document).ready(function() {
-}
+
+$(document).ready(function(){
+  $("form#robotner").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const number = replaceNumber(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#your-number").text(replacedArray);
+    
+  });
+});
